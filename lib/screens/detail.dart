a3f1ff1e036d4a2707b040f1058cc7812f4d5db7
black-tul1p <divay7@gmail.com>
2021-10-21 10:11:46 -0400

    Fixed imports

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:toggle_switch/toggle_switch.dart';
 import '../clothes.dart';
 
 class DetailPage extends StatefulWidget {
--- lib/screens/laundry/laundry.dart
+++ lib/screens/laundry/laundry.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:virtual_closet/models/user.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 import '../../service/database.dart';
 import '../../clothes.dart';
 import '../detail.dart';
