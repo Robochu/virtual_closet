f45a78c74e2bfd6742de914c8524b53c9041751a
Meghna <mrajago@purdue.edu>
2021-11-26 19:56:33 -0500

    AI now works correctly and fills in the details based on the image. We should probably tweak the code a bit when Suits gets a category.

--- lib/screens/camera_screen/image_gallery.dart
+++ lib/screens/camera_screen/image_gallery.dart
@@ -14,6 +14,7 @@ import 'package:virtual_closet/screens/detail.dart';
 import 'package:firebase_ml_custom/firebase_ml_custom.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
+import 'package:collection/collection.dart';
 //import 'package:tflite/tflite.dart';
 
 class ImageFromGalleryScreen extends StatefulWidget {
@@ -30,6 +31,7 @@ class ImageFromGalleryScreenState extends State<ImageFromGalleryScreen> {
   var _image;
   var imagePicker;
   var type;
+  var _user;
 
   late Interpreter interpreter;
   late InterpreterOptions _interpreterOptions;
@@ -43,12 +45,17 @@ class ImageFromGalleryScreenState extends State<ImageFromGalleryScreen> {
   late TfLiteType _inputType;
   late TfLiteType _outputType;
 
+  late List<String> labels;
+  final int _labelsLength = 1001;
+
   final String _labelsFileName = 'assets/dict.txt';
 
-  final String _modelFilePath = 'assets/model-export_icn_tflite-Bingabad_20211104015315-2021-11-14T17_06_32.199644Z_model.tflite';
+  final String _modelFilePath = 'model-export_icn_tflite-Bingabad_20211104015315-2021-11-14T17_06_32.199644Z_model.tflite';
 
   NormalizeOp preProcessNormalizeOp = NormalizeOp(0, 1);
   NormalizeOp postProcessNormalizeOp = NormalizeOp(0, 255);
+
+  late var _probabilityProcessor;
 
   ImageFromGalleryScreenState(this.type);
 
@@ -61,8 +68,9 @@ class ImageFromGalleryScreenState extends State<ImageFromGalleryScreen> {
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<MyUser?>(context);
-    //_interpreterOptions = InterpreterOptions();
-    loadModel();
+    _user = user;
+    _interpreterOptions = InterpreterOptions();
+    _interpreterOptions.threads = 1;
     var title;
     var source;
     if (type == 'camera') {
@@ -74,17 +82,73 @@ class ImageFromGalleryScreenState extends State<ImageFromGalleryScreen> {
     }
     void up() async {
       try {
-        predict();
-        print(_outputBuffer);
-        /*List<dynamic>? recognitions = await Tflite.runModelOnImage(path: _image.path);
-        if (recognitions == null) {
-          print("ERROR");
+        loadModel();
+        loadLabels();
+        String prediction = predict();
+        print("PREDICTION: " + prediction);
+        String category;
+        String length;
+        String color;
+        String itemName;
+
+        if (prediction.indexOf("hat") != -1) {
+          category = "Accessories";
+          itemName = "Hat";
+          length = "N/A";
+          color = prediction.substring(0, prediction.indexOf(' '));
+          color = color[0].toUpperCase() + color.substring(1);
+        }
+        else if (prediction.indexOf("jacket") != -1) {
+          category = "Outerwear";
+          itemName = "Jacket";
+          length = "Long";
+          color = prediction.substring(0, prediction.indexOf(' '));
+          color = color[0].toUpperCase() + color.substring(1);
+        }
+        else if (prediction.indexOf("pants") != -1) {
+          category = "Bottoms";
+          itemName = "Pants";
+          length = "Long";
+          color = prediction.substring(0, prediction.indexOf(' '));
+          color = color[0].toUpperCase() + color.substring(1);
+        }
+        else if (prediction.indexOf("shoe") != -1) {
+          category = "Shoes";
+          itemName = "Shoes";
+          length = "N/A";
+          color = prediction.substring(0, prediction.indexOf(' '));
+          color = color[0].toUpperCase() + color.substring(1);
+        }
+        else if (prediction.indexOf("shorts") != -1) {
+          category = "Bottoms";
+          itemName = "Shorts";
+          length = "Short";
+          color = prediction.substring(0, prediction.indexOf(' '));
+          color = color[0].toUpperCase() + color.substring(1);
+        }
+        else if (prediction.indexOf("suit") != -1) {
+          category = "Tops";
+          itemName = "Suit";
+          length = "Long";
+          color = prediction.substring(0, prediction.indexOf(' '));
+          color = color[0].toUpperCase() + color.substring(1);
+        }
+        else if (prediction.indexOf("t-shirt") != -1) {
+          category = "Tops";
+          itemName = "T-shirt";
+          length = "Short";
+          color = prediction.substring(0, prediction.indexOf(' '));
+          color = color[0].toUpperCase() + color.substring(1);
         }
         else {
-          print("Recognitions Length: ");
-          print(recognitions.length);
-        }*/
-        Clothing item = Clothing.full(user!.uid, _image.path, '', '', 'Tops', 'Short', 'Black', '', 'T-shirt', false, '');
+          category = "Accessories";
+          itemName = "Other";
+          length = "N/A";
+          color = "Multicolor";
+        }
+
+        //Clothing item = Clothing.full(user!.uid, _image.path, '', '', 'Tops', 'Short', 'Black', '', 'T-shirt', false, '');
+        Clothing item = Clothing.full(user!.uid, _image.path, '', '', category, length, color, '', itemName, false, '');
         //Tflite.close();
         close();
         Navigator.push(
@@ -94,6 +158,7 @@ class ImageFromGalleryScreenState extends State<ImageFromGalleryScreen> {
       } catch (exception) {
         print("Failed on getting your image and it's labels: $exception");
         print('Continuing with the program...');
+        _showMyDialog();
         rethrow;
       }
     }
@@ -159,8 +224,8 @@ class ImageFromGalleryScreenState extends State<ImageFromGalleryScreen> {
 
   Future<void> loadModel() async {
     try {
-      interpreter =
-      await Interpreter.fromAsset(_modelFilePath, options: _interpreterOptions);
+      print("LOADING\nTHE\nMODEL\n");
+      interpreter = await Interpreter.fromAsset(_modelFilePath, options: _interpreterOptions);
       print('Interpreter Created Successfully');
 
       _inputShape = interpreter.getInputTensor(0).shape;
@@ -169,8 +234,21 @@ class ImageFromGalleryScreenState extends State<ImageFromGalleryScreen> {
       _outputType = interpreter.getOutputTensor(0).type;
 
       _outputBuffer = TensorBuffer.createFixedSize(_outputShape, _outputType);
+
+      _probabilityProcessor =
+          TensorProcessorBuilder().add(postProcessNormalizeOp).build();
+
     } catch (e) {
       print('Unable to create interpreter, Caught Exception: ${e.toString()}');
+    }
+  }
+
+  Future<void> loadLabels() async {
+    labels = await FileUtil.loadLabels(_labelsFileName);
+    if (labels.length == _labelsLength) {
+      print('Labels loaded successfully');
+    } else {
+      print('Unable to load labels');
     }
   }
 
@@ -178,22 +256,154 @@ class ImageFromGalleryScreenState extends State<ImageFromGalleryScreen> {
     int cropSize = min(_inputImage.height, _inputImage.width);
     return ImageProcessorBuilder()
         .add(ResizeWithCropOrPadOp(cropSize, cropSize))
-        .add(ResizeOp(
-        _inputShape[1], _inputShape[2], ResizeMethod.NEAREST_NEIGHBOUR))
+        .add(ResizeOp(_inputShape[1], _inputShape[2], ResizeMethod.NEAREST_NEIGHBOUR))
         .build()
         .process(_inputImage);
   }
 
-  void predict() {
-    _inputImage = TensorImage(_inputType);
-    _inputImage.loadImage(_image);
+  String predict() {
+    _inputImage = TensorImage.fromFile(_image);
+    /*_inputImage = TensorImage(_inputType);
+    imageImage = Image.file(_image);
+    _inputImage.loadImage(Image.file(_image));*/
     _inputImage = _preProcess();
     interpreter.run(_inputImage.buffer, _outputBuffer.getBuffer());
+    Map<String, double> labeledProb = TensorLabel.fromList(
+        labels, _probabilityProcessor.process(_outputBuffer))
+        .getMapWithFloatValue();
+    final pred = getTopProbability(labeledProb);
+    return pred.key;
   }
 
   void close() {
     interpreter.close();
   }
+
+  MapEntry<String, double> getTopProbability(Map<String, double> labeledProb) {
+    var pq = PriorityQueue<MapEntry<String, double>>(compare);
+    pq.addAll(labeledProb.entries);
+
+    return pq.first;
+  }
+
+  int compare(MapEntry<String, double> e1, MapEntry<String, double> e2) {
+    if (e1.value > e2.value) {
+      return -1;
+    } else if (e1.value == e2.value) {
+      return 0;
+    } else {
+      return 1;
+    }
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Are you sure you want to use this image?'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Yes'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                goToDetails();
+              },
+            ),
+            TextButton(
+              child: const Text('No'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void goToDetails() async {
+    try {
+      loadModel();
+      loadLabels();
+      String prediction = predict();
+      print("PREDICTION: " + prediction);
+      String category;
+      String length;
+      String color;
+      String itemName;
+
+      if (prediction.indexOf("hat") != -1) {
+        category = "Accessories";
+        itemName = "Hat";
+        length = "N/A";
+        color = prediction.substring(0, prediction.indexOf(' '));
+        color = color[0].toUpperCase() + color.substring(1);
+      }
+      else if (prediction.indexOf("jacket") != -1) {
+        category = "Outerwear";
+        itemName = "Jacket";
+        length = "Long";
+        color = prediction.substring(0, prediction.indexOf(' '));
+        color = color[0].toUpperCase() + color.substring(1);
+      }
+      else if (prediction.indexOf("pants") != -1) {
+        category = "Bottoms";
+        itemName = "Pants";
+        length = "Long";
+        color = prediction.substring(0, prediction.indexOf(' '));
+        color = color[0].toUpperCase() + color.substring(1);
+      }
+      else if (prediction.indexOf("shoe") != -1) {
+        category = "Shoes";
+        itemName = "Shoes";
+        length = "N/A";
+        color = prediction.substring(0, prediction.indexOf(' '));
+        color = color[0].toUpperCase() + color.substring(1);
+      }
+      else if (prediction.indexOf("shorts") != -1) {
+        category = "Bottoms";
+        itemName = "Shorts";
+        length = "Short";
+        color = prediction.substring(0, prediction.indexOf(' '));
+        color = color[0].toUpperCase() + color.substring(1);
+      }
+      else if (prediction.indexOf("suit") != -1) {
+        category = "Tops";
+        itemName = "Suit";
+        length = "Long";
+        color = prediction.substring(0, prediction.indexOf(' '));
+        color = color[0].toUpperCase() + color.substring(1);
+      }
+      else if (prediction.indexOf("t-shirt") != -1) {
+        category = "Tops";
+        itemName = "T-shirt";
+        length = "Short";
+        color = prediction.substring(0, prediction.indexOf(' '));
+        color = color[0].toUpperCase() + color.substring(1);
+      }
+      else {
+        category = "Accessories";
+        itemName = "Other";
+        length = "N/A";
+        color = "Multicolor";
+      }
+      Clothing item = Clothing.full(_user!.uid, _image.path, '', '', category, length, color, '', itemName, false, '');
+      //Clothing item = Clothing.full(_user!.uid, _image.path, '', '', 'Tops', 'Short', 'Black', '', 'T-shirt', false, '');
+      //Tflite.close();
+      close();
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) =>
+              DetailPage(clothing: item, editable: true)));
+    } catch (exception) {
+      print("Failed on getting your image and it's labels: $exception");
+      print('Continuing with the program...');
+      rethrow;
+    }
+  }
+
 
 
 
