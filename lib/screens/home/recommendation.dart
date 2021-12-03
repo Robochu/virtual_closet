28f15b672b3be88c30682e4eda3c162383d8cf05
Robochu <oleg.aprelikov@gmail.com>
2021-12-02 20:15:20 -0500

    Started recommending outfits, although the recommended items flash for some reason.

--- lib/screens/home/recommendation.dart
+++ lib/screens/home/recommendation.dart
@@ -1,5 +1,7 @@
 import 'package:virtual_closet/clothes.dart';
 import 'package:collection/collection.dart';
+import 'package:virtual_closet/screens/combinations/outfit.dart';
+import 'package:virtual_closet/service/database.dart';
 
 class Recommendation {
   int score;
@@ -12,11 +14,25 @@ class Recommendation {
 class RecommendationQueue {
   List<Clothing> closet;
   List<String> attributes;
+  List<Outfit> outfits;
+  List<Clothing> blacklist = [];
+  List<Clothing> whitelist = [];
   List<Recommendation> recommendations = <Recommendation>[];
-  RecommendationQueue({required this.closet, required this.attributes}) {
-      for (var item in closet) {
-        recommendations.add(Recommendation(score: 0, clothing: item));
-
+  RecommendationQueue({required this.closet, required this.attributes, required this.outfits}) {
+    for (var item in closet) {
+      print(item.link);
+      recommendations.add(Recommendation(score: 0, clothing: item));
+    }
+    print('');
+    for (Outfit outfit in outfits) {
+      if (outfit.recommendationDate != null &&
+        outfit.recommendationDate!.difference(DateTime.now()).inDays <= 7) {
+        if (outfit.recommendationDate!.difference(DateTime.now()).inDays == 0) {
+          whitelist.addAll(outfit.clothes);
+        } else {
+          blacklist.addAll(outfit.clothes);
+        }
+      }
     }
   }
 
@@ -31,6 +47,14 @@ class RecommendationQueue {
 
   void calculateScore() {
     for(var item in recommendations) {
+      if (blacklist.contains(item.clothing)) {
+        item.score = 0;
+        continue;
+      } else if (whitelist.contains(item.clothing)) {
+        item.score = 20;
+        continue;
+      }
+
       if(attributes.contains(item.clothing.category)) {
         item.score = item.score+ 1;
       }
